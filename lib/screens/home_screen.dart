7ef1870de5821f71af55518293606f348ee21728
schorio <schorioignace@gmail.com>
2022-11-27 19:34:06 +0300

    Resolution build apres pub get

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,12 +18,64 @@ class HomeScreen extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+        appBar: const _CustomAppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.deepPurple.shade800,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline),
+              label: 'Favorites'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline),
+              label: 'Play'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Profile'
+            ),
+          ]
         ),
         body: Container(),
       ),
     );
   }
+}
+
+class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const _CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: Image.asset('assets/images/scho.jpg'),
+          ),
+        )
+      ],
+    );
+  }
+  
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
 }
