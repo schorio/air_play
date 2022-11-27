e05d34301b7280ade55961ac3405719a7df9552b
schorio <schorioignace@gmail.com>
2022-11-27 20:27:56 +0300

    Finalisation de widget contenant le Message et la barre de recherche

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,30 +21,70 @@ class HomeScreen extends StatelessWidget {
         appBar: const _CustomAppBar(),
         bottomNavigationBar: const _CustomNavBar(),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Bonjour',
-                  style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'Profiter de votre musique preferer',
-                  style: Theme.of(context)
-                            .textTheme
-                            .headline6!
-                            .copyWith(fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
+          child: Column(
+            children: [
+              const _DiscoverMusic(),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _DiscoverMusic extends StatelessWidget {
+  const _DiscoverMusic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Bonjour',
+            style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge
+          ),
+
+          const SizedBox(height: 5),
+
+          Text(
+            'Profiter de votre musique preferer',
+            style: Theme.of(context)
+                      .textTheme
+                      .headline6!
+                      .copyWith(fontWeight: FontWeight.bold),
+          ),
+
+          const SizedBox(height: 20),
+
+          TextFormField(
+            decoration: InputDecoration(
+              isDense: true,
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Rechercher',
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.grey.shade400),
+              prefix: Icon(
+                Icons.search, 
+                color: Colors.grey.shade400, 
+                size: 18.0
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: BorderSide.none
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
