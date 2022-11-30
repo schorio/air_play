e9f2203ab352c6fcd8ff9eb27b8010499843d4b0
schorio <schorioignace@gmail.com>
2022-11-30 09:51:45 +0300

    Extraction du Widget contenent le top Music

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,37 +30,51 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               const _DiscoverMusic(),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20.0,
-                  top: 20.0,
-                  bottom: 20.0,
-                ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: SectionHeader(title: 'Top Music'),
-                    ),
-
-                    const SizedBox(height: 20),
-                    
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.27,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: songs.length,
-                        itemBuilder: (context, index) {
-                          return SongCard(song: songs[index]);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              _TrendingMusic(songs: songs),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _TrendingMusic extends StatelessWidget {
+  const _TrendingMusic({
+    Key? key,
+    required this.songs,
+  }) : super(key: key);
+
+  final List<Song> songs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        top: 20.0,
+        bottom: 20.0,
+      ),
+      child: Column(
+        children: [
+          const Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: SectionHeader(title: 'Top Music'),
+          ),
+
+          const SizedBox(height: 20),
+          
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.27,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: songs.length,
+              itemBuilder: (context, index) {
+                return SongCard(song: songs[index]);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
