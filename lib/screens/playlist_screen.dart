f224228e876a5120d7070faedfd6321514b6b219
schorio <schorioignace@gmail.com>
2022-12-10 16:44:40 +0300

    Barre des infos du playlist

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -7,7 +7,7 @@ class PlaylistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Playlist> playlists = Playlist.playlists;
+    Playlist playlist = Playlist.playlists[0];
 
     return Container(
       decoration: BoxDecoration(
@@ -28,13 +28,54 @@ class PlaylistScreen extends StatelessWidget {
           title: const Text('Playlist'),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                _PlaylistInformation(playlist: playlist),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PlaylistInformation extends StatelessWidget {
+  const _PlaylistInformation({
+    Key? key,
+    required this.playlist,
+  }) : super(key: key);
+
+  final Playlist playlist;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Image.asset(
+            playlist.imageUrl,
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.height * 0.3,
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        const SizedBox(height: 30),
+
+        Text(
+          playlist.title,
+          style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(
+                      fontWeight: FontWeight.bold
+                    ),
+        ),
+      ],
     );
   }
 }
