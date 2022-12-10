df5444ceea19e9cfcb957caef50238cdc8e2febd
schorio <schorioignace@gmail.com>
2022-12-10 12:29:38 +0300

    Creation du slider

--- lib/widgets/seekbar.dart
+++ lib/widgets/seekbar.dart
@@ -30,6 +30,33 @@ class SeekBar extends StatefulWidget {
 class _SeekBarState extends State<SeekBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: SliderTheme(
+            data: SliderTheme
+                    .of(context)
+                    .copyWith(
+                      trackHeight: 4,
+                      thumbShape: const RoundSliderThumbShape(
+                        disabledThumbRadius: 4,
+                        enabledThumbRadius: 4,
+                      ),
+                      overlayShape: const RoundSliderOverlayShape(
+                        overlayRadius: 10
+                      ),
+                      activeTrackColor: Colors.white.withOpacity(0.2),
+                      inactiveTrackColor: Colors.white,
+                      thumbColor: Colors.white,
+                      overlayColor: Colors.white
+                    ),
+            child: Slider(
+              value: 0, 
+              onChanged: (value) {},
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
