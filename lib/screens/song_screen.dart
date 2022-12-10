4864c8ea7769ce25d72b3dc748851408e8290daf
schorio <schorioignace@gmail.com>
2022-12-10 11:16:15 +0300

    Gradient du pochette

--- lib/screens/song_screen.dart
+++ lib/screens/song_screen.dart
@@ -20,8 +20,51 @@ class SongScreen extends StatelessWidget {
           Image.asset(
             song.coverUrl,
             fit: BoxFit.cover,
-          )
+          ),
+
+          const _BackgroundFilter(),
         ],
+      ),
+    );
+  }
+}
+
+class _BackgroundFilter extends StatelessWidget {
+  const _BackgroundFilter({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) {
+        return LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.white.withOpacity(0.9),
+              Colors.white.withOpacity(0.1),
+            ],
+            stops: const [
+              0.0,
+              0.4 ,
+              0.7
+            ]
+          ).createShader(rect);
+      },
+      blendMode: BlendMode.dstOut,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade800,
+            ]
+          )
+        ),
       ),
     );
   }
