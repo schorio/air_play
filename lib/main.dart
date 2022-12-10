e757754cb3b6f6b111dfa1421329ff28e64bfcd5
schorio <schorioignace@gmail.com>
2022-12-10 15:59:52 +0300

    Modifier la page d'accueil comme HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           displayColor: Colors.white,
         )
       ),
-      home: const SongScreen(),
+      home: const HomeScreen(),
       getPages: [
         GetPage(name: '/', page: () => const HomeScreen()),
         GetPage(name: '/song', page: () => const SongScreen()),
