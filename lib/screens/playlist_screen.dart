a0051eb101b19055afb1b4997cf3822468396494
schorio <schorioignace@gmail.com>
2022-12-10 17:26:41 +0300

    Ajout du boutton play du playlist

--- lib/screens/playlist_screen.dart
+++ lib/screens/playlist_screen.dart
@@ -33,9 +33,97 @@ class PlaylistScreen extends StatelessWidget {
             child: Column(
               children: [
                 _PlaylistInformation(playlist: playlist),
+                const SizedBox(height: 30),
+                _PlayOrShuffleSwitch()
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayOrShuffleSwitch extends StatefulWidget {
+  const _PlayOrShuffleSwitch({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_PlayOrShuffleSwitch> createState() => _PlayOrShuffleSwitchState();
+}
+
+class _PlayOrShuffleSwitchState extends State<_PlayOrShuffleSwitch> {
+  bool isPlay = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isPlay = !isPlay;
+        });
+      },
+      child: Container(
+        height: 50,
+        width: width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Stack(
+          children: [ 
+
+            
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Play',
+                          style: TextStyle(
+                            color: isPlay ? Colors.white : Colors.deepPurple,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Icon(
+                        Icons.play_circle,
+                        color: isPlay ? Colors.white : Colors.deepPurple,
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Shuffle',
+                          style: TextStyle(
+                          color: isPlay ? Colors.deepPurple : Colors.white,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Icon(
+                        Icons.shuffle,
+                        color: isPlay ? Colors.deepPurple : Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ), 
+          ],
         ),
       ),
     );
